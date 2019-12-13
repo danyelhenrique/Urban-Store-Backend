5ebafba18492815deed4972c3a81e956dd3b223e
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-12-12 22:35:33 -0300

    clear migration products to modify

--- src/database/migrations/20191203010319-products.js
+++ src/database/migrations/20191203010319-products.js
@@ -9,31 +9,7 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER
             },
-            name: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            price: {
-                allowNull: false,
-                type: Sequelize.DECIMAL
-            },
-            colors: {
-                type: Sequelize.JSONTYPE,
-                allowNull: false
-            },
-            image_url: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            description: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            created_at: {
-                allowNull: false,
-                type: 'TIMESTAMP',
-                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-            },
+         
             updated_at: {
                 allowNull: false,
                 type: 'TIMESTAMP',
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('./config/mongoDb')
 require('./database')
 
 const express = require('express')
+const { resolve } = require('path')
 const { ApolloServer } = require('apollo-server-express')
 const UserConnect = require('./app/connects/user')
 const ProductConnect = require('./app/connects/product')
@@ -13,7 +14,7 @@ const resolvers = require('./app/resolvers')
 
 const server = new ApolloServer({
     typeDefs,
-resolvers,
+    resolvers,
     context: async ({ req }) => {
         return {
             helpers: { UserConnect, ProductConnect, PurchaseConnect }
@@ -24,11 +25,18 @@ resolvers,
 const app = express()
 const PORT = process.env.PORT || 3333
 
+app.use(express.static('./public/models'))
 app.use(express.urlencoded({ extended: true }))
+
+const image = resolve(__dirname, './public/models')
 
 const path = '/graphql'
 
 server.applyMiddleware({ app, path })
+
+app.get('/oi', (req, res) => {
+    res.sendfile(image + '/boy1.jpg')
+})
 
 app.listen({ port: PORT }, () =>
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
