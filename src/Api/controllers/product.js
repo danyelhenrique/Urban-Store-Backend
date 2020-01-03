83cacbe4084292fcc847e9042d40fbaab721a1d0
unknown <danyelhenriquefidel@gmail.com>
2020-01-02 22:34:15 -0300

    change package.json and babelrc

--- src/Api/controllers/product.js
+++ src/Api/controllers/product.js
@@ -1,39 +1,46 @@
-import ProductModel from '../models/Product'
+import ProductModel from '../models/Product';
 
 class Product {
-	async index({ offset, limit }) {
-		const products = await ProductModel.findAll({})
-		return products.map((product) => {
-			return product.dataValues
-		})
+	async index({ page, limit }) {
+		const checkPage = page >= 0 ? 1 : page;
+		const offset = (checkPage - 1) * limit;
+		const products = await ProductModel.findAll({
+			limit,
+			offset
+		});
+		//return products.map((product) => {
+		//	console.log({...product.dataValues});
+			//return [{...product.dataValues}]
+		//});
+		 return  products ;
 	}
 
 	async show({ id }) {
-		const product = await ProductModel.findByPk(id)
-		return product.dataValues
+		const product = await ProductModel.findByPk(id);
+		return product.dataValues;
 	}
 
 	async store({ input }) {
 		const product = await ProductModel.create({
 			input
-		})
-		return product.dataValues
+		});
+		return product.dataValues;
 	}
 
 	async update({ id, input }) {
-		const updateProduct = await ProductModel.findByPk(id)
+		const updateProduct = await ProductModel.findByPk(id);
 
-		const product = await updateProduct.update(input)
+		const product = await updateProduct.update(input);
 
-		return product
+		return product;
 	}
 
 	async destroy({ id }) {
-		const destroyProduct = await ProductModel.findByPk(id)
+		const destroyProduct = await ProductModel.findByPk(id);
 
-		const product = await destroyProduct.destroy()
-		return !product.dataValues
+		const product = await destroyProduct.destroy();
+		return !product.dataValues;
 	}
 }
 
-export default new Product()
+export default new Product();
--- src/Graphql/query/product.js
+++ src/Graphql/query/product.js
@@ -1,10 +1,10 @@
-import { gql } from 'apollo-server-express'
+import { gql } from 'apollo-server-express';
 const typeQuery = gql`
 	directive @isAuthenticate on FIELD_DEFINITION
 
 	extend type Query {
-		indexProduct(offset: String, limit: String): [Product]
+		indexProduct(page: Int, limit: Int): [Product]
 		showProduct(id: ID!): Product @isAuthenticate
 	}
-`
-export default typeQuery
+`;
+export default typeQuery;
