b07da777927cbeaa5251de5c8279a139eb5cea66
unknown <danyelhenriquefidel@gmail.com>
2020-02-15 00:13:26 -0300

    --fix conflict database

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,7 +1,6 @@
 import './dotenv'
 
 module.exports = {
-<<<<<<< HEAD
     username: process.env.DATABASE_USER_NAME,
     password: process.env.DATABASE_USER_PASSWORD,
     database: process.env.DATABASE_NAME,
@@ -12,19 +11,4 @@ module.exports = {
         createdAt: 'created_at',
         updatedAt: 'updated_at'
     }
-=======
-	username: process.env.DATABASE_USER_NAME,
-	password: process.env.DATABASE_USER_PASSWORD,
-	database: process.env.DATABASE_NAME,
-	host: process.env.DATABASE_HOST,
-	dialect: process.env.DATABASE_DIALECT,
-    dialectOptions: {
-    ssl: true
-    },
-	define: {
-		timestamps: true,
-		createdAt: 'created_at',
-		updatedAt: 'updated_at'
-	}
->>>>>>> bb4bc3d1cabb8f7db52f4315948003a732b3d7a1
 }
